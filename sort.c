b668b4cb12abd93127a2e0466e6a71534fda6134
will2869 <will2869@purdue.edu>
2026-01-20 13:24:28 -0500

    comments added

--- sort.c
+++ sort.c
@@ -5,18 +5,19 @@
 void ssort(int * arr, int size) {
 	/* For step 3, fill this in to perform a selection sort
 	   For step 4, add conditional compilation flags to perform an ascending selection sort instead */
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < size; i++) { // for loop through array
 		
-		int minIdx = i;
+		int minIdx = i; // minimum index
 		
-		for (int j = (i + 1); j < size; j++) {
-			#ifdef ASCENDING
-			minIdx = (arr[j] < arr[minIdx]) ? j : minIdx;
+		for (int j = (i + 1); j < size; j++) { // loop through values after current index
+			#ifdef ASCENDING // if ascending defined
+			minIdx = (arr[j] < arr[minIdx]) ? j : minIdx; // if current index is < minimum, set new minimum
 			#else
-			minIdx = (arr[j] > arr[minIdx]) ? j : minIdx;
+			minIdx = (arr[j] > arr[minIdx]) ? j : minIdx; // if current index is > minimum, set new minimum
 			#endif
 		}
-
+		
+		// swap current and minimum
 		int swap = arr[i];
 		arr[i] = arr[minIdx];
 		arr[minIdx] = swap;
